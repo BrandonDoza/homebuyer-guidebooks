c2abffafdf3d8ea8bd8fb0a96d973ffe50fc7b39
Brandon Doza <141676838+BrandonDoza@users.noreply.github.com>
2025-07-23 11:34:16 -0600

    add Ryan Liebelt and Ashley Strickland

--- src/lib/lenders.ts
+++ src/lib/lenders.ts
@@ -1,10 +1,19 @@
 // lender directory
 
-export const lenderDirectory: Record<string, { fullName: string; contactUrl: string; firstName: string; lastName: string }> = {
-    brandondoza: {
-      fullName: "Brandon Doza",
-      firstName: "Brandon",
-      lastName: "Doza",
-      contactUrl: "https://www.homeloanswithbrandon.com",
-    },
-  }
+export const lenderDirectory: Record<
+  string,
+  { fullName: string; contactUrl: string; firstName: string; lastName: string }
+> = {
+  brandondoza: {
+    fullName: "Brandon Doza",
+    firstName: "Brandon",
+    lastName: "Doza",
+    contactUrl: "https://www.homeloanswithbrandon.com",
+  },
+  ryanliebelt: {
+    fullName: "Ryan Liebelt",
+    firstName: "Ryan",
+    lastName: "Liebelt",
+    contactUrl: "https://www.loanswithryan.com/",
+  },
+};
